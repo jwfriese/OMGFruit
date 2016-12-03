851dfdb314bd797ccf412610b7f598982ff98f1d
Jared Friese <jared.friese@gmail.com>
2016-12-02 19:33:00 -0800

    Test go get troubles

--- .concourse.test.sh
+++ .concourse.test.sh
@@ -3,7 +3,8 @@
 set -e -x
 
 export GOPATH=~
-go get github.com/jwfriese/iossimulator
+go get -u github.com/jwfriese/iossimulator
+ls $GOPATH/src/github.com/jwfriese/iossimulator
 go build script/test.go
 carthage update --platform 'iOS'
 ./test "iOS 10.0" "iPhone 6" "false"
